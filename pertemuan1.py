c88fb4ee91a77a72914fd6a7e9eee3381e8942da
Wisnu_Alprog-C <wisnutazridhuwisnu@gmail.com>
2025-11-03 17:45:40 +0800

    Add files via upload

---
+++ pertemuan1.py
@@ -0,0 +1,15 @@
+print("nama wisnu")
+print("kuliah di undikma")
+print("umur 18")
+print("nim:25241005")  
+print("kelas: pti c")
+#memindahkan baris alt+atas atau bawah
+#cara copy satu baris arahkan kursor lalu tahan alt+shift
+#cara copy satu baris arahkan kursor lalu tahan alt+shift
+#cara copy satu baris arahkan kursor lalu tahan alt+shift
+#cara copy satu baris arahkan kursor lalu tahan alt+shift
+a=7
+b=8
+print(a+b)
+
+
