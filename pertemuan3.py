704412264f2dd1abd83d8ccce9050019a01b5488
Wisnu_Alprog-C <wisnutazridhuwisnu@gmail.com>
2025-11-06 14:59:17 +0800

    Add files via upload

---
+++ pertemuan3.py
@@ -0,0 +1,29 @@
+# variabel = cara kita menyimpan nilai/velue(aljabar)
+# nilai x adalah varibel dan = adalah assigment dan 13 itu nilai value
+
+x=3
+y=2
+lebar=1
+
+print ("nilai x adalah: ", x)
+print ("nilai y adalah: ", y)
+print ("nilai lebar adalah: ",lebar)
+# aturan penanaman variabel 
+bet99 = 100 # tidak boleh diawali angka 
+wisnu_ganteng = 100 # jika lebih dari 1 kata, gabung dengan_
+jumlahhutang = 1000000 # bedakan dengan kapital 
+
+# panggilan indirect
+z=x
+
+print(z)
+print("nilai z adalah: ",z)
+print("nilai y adalah: ",y)
+print("nilai lebar adalah: ",lebar)
+
+a ="wisnu_tazridhu"
+b = "25241005"
+print("nama saya adalah:wisnu_tazridhu",)
+print("nim saya adalah:25241005 ",)
+
+                            
